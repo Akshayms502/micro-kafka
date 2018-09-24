4751f07d5c12ad5d3da7be36ec6f19c87db48ad5
Akshay ms <akshayms502@gmail.com>
2018-09-24 20:04:38 +0530

    spark

--- SparksBasics/src/com/mongo/ReadMongo.java
+++ SparksBasics/src/com/mongo/ReadMongo.java
@@ -1,19 +1,39 @@
 package com.mongo;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.bson.Document;
 
 import com.mongodb.spark.MongoSpark;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import static java.util.Arrays.asList;
 
 
 
 public class ReadMongo {
 	public static void main(String[] args) {
 		JavaSparkContext mongoContext=MongoSparkConfiguration.getContext();
-		JavaMongoRDD<Document> val=MongoSpark.load(mongoContext);
-		val.take(2).forEach(System.out::println);;
 		
+		//mongo read operation
+		JavaMongoRDD<Document> readData=MongoSpark.load(mongoContext);
+		readData.take(2).forEach(System.out::println);;
+	
+		//mongo write operation
+		JavaRDD<Document> writeData=mongoContext.parallelize(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+				.map(new Function<Integer, Document>() {
+
+					@Override
+					public Document call(Integer val) throws Exception {
+						
+						return Document.parse("{test: " + val + "}");
+					}
+				});
+		
+		MongoSpark.save(writeData);
 	}
 }
