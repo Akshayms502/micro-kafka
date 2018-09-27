f767478c332574b10ce1d26dcd6480e89f848804
Akshay ms <akshayms502@gmail.com>
2018-09-27 17:57:20 +0530

    S

--- Server/src/main/java/com/micro/Server/ServerApplication.java
+++ Server/src/main/java/com/micro/Server/ServerApplication.java
@@ -3,12 +3,10 @@ package com.micro.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableConfigServer
 @EnableAutoConfiguration
 public class ServerApplication {
 
